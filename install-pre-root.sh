a354d9fc3cfb9c0db47b97c1d39c5796830b9552
kub4e <61429531+kub4e@users.noreply.github.com>
2021-09-04 11:24:31 +0000

    Update install-pre-root.sh

--- install-pre-root.sh
+++ install-pre-root.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/sh
+mkfs.ext4 /dev/sda2
+mkfs.fat -F32 /dev/sda1
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 
 timedatectl set-ntp true
 pacstrap /mnt base base-devel linux linux-firmware efibootmgr networkmanager zsh texinfo amd-ucode sudo dhcpcd dhclient vim git man-db man-pages openssh wget
